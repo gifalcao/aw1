b6e01ada2e2f17305ba717d0103b56df6cebdff0
Giovanna Falcão <78114184+gifalcao@users.noreply.github.com>
2021-08-20 09:39:02 -0300

    Create level7.js

---
+++ atividades/A11/level7.js
@@ -0,0 +1,15 @@
+function init(robot) {
+    console.log("Robot starting")
+    robot.oldCoins = 0;
+    robot.oldEnergy = 100;
+}
+
+function loop(robot) {
+    robot.action = { type: 'move', amount: 40 };
+    if (robot.info().coins > robot.oldCoins ||
+        robot.info().energy > robot.oldEnergy) {
+        robot.action = { type: 'jump', amount: 10 };
+    }
+    robot.oldCoins = robot.info().coins;
+    robot.oldEnergy = robot.info().energy;
+}
